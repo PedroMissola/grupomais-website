0ec73017eed754b20d74496b65f53eb7197ca267
PedroMissola <missolapedro@gmail.com>
2025-06-19 21:22:31 -0300

    fix: Comment out unused Mapbox CSS import to clean up code

--- src/app/components/contact/map/index.jsx
+++ src/app/components/contact/map/index.jsx
@@ -2,7 +2,7 @@
 
 import { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+// import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 

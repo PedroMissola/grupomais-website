cc82261301b88f3253c6d7ee21ea499798f3cc96
PedroMissola <missolapedro@gmail.com>
2025-06-24 15:49:14 -0300

    fix: remove extra whitespace in error message and update button styles for consistency

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -14,12 +14,12 @@ export default function ErrorSection7() {
             Erro 404 <br /> Parece que não encontramos o que você procura
           </h2>
           <p className="mb-12 text-foreground max-w-xl [text-wrap:balance] mx-auto text-lg">
-            A página que você está tentando acessar não existe ou foi removida. 
+            A página que você está tentando acessar não existe ou foi removida.
             Verifique o endereço digitado ou volte para a página inicial.
           </p>
           <Link
-            href="/" 
-            className="inline-flex border font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-base rounded-lg py-3 px-6 shadow-sm hover:shadow-lg bg-blue-800 border-blue-800 text-neutral-50 hover:bg-blue-600 hover:border-blue-600"
+            href="/"
+            className="inline-flex border font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-base rounded-lg py-3 px-6 shadow-sm hover:shadow-lg bg-[#113548] border-[#113548] text-neutral-50 hover:bg-[#274250] hover:border-[#274250]"
           >
             Voltar a página inicial
           </Link>

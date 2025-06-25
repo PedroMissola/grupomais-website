13bc029f7ea6779b298b98f8b9aa17a39a3e0762
PedroMissola <missolapedro@gmail.com>
2025-06-25 18:12:34 -0300

    fix: adjust button styling and icon size in ButtonHome component

--- src/app/components/ui/homeButton/index.jsx
+++ src/app/components/ui/homeButton/index.jsx
@@ -8,10 +8,9 @@ export default function ButtonHome() {
             <div className="fixed bottom-4 right-4 flex flex-col items-end gap-3 z-50">
                 <Link
                     href="/"
-                    className="flex items-center gap-2 bg-[#113548] text-white px-4 py-2 rounded-xl shadow hover:bg-[#0a1e29] transition"
+                    className="flex items-center gap-2 bg-[#113548] text-white px-4 py-4 mx-4 my-2 rounded-xl shadow hover:bg-[#0a1e29] transition"
                 >
-                    <Home className="w-4 h-4" />
-                    Home
+                    <Home className="w-6 h-6" />
                 </Link>
             </div>
         </>
